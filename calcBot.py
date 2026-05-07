bef7bad0ce6d3caa670691c0466850e5058f44cd
Captain-dd <dhruvildholariya111@gmail.com>
2026-05-07 11:42:10 +0530

    added if __name__

--- calcBot.py
+++ calcBot.py
@@ -104,3 +104,7 @@ def webhook():
         print("ERROR:", e)
 
     return "ok", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5007))
+    app.run(host="0.0.0.0", port=port)
